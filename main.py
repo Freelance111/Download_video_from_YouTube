f28abe30d0a5ece5266455ed15d8ed290e534287
Maxwell012 <upw18506@gmail.com>
2023-03-18 22:04:59 +0100

    Upgrade the old code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pytube import YouTube, Playlist
 from threading import Thread
 
+
 def main():
     format = input('1 - file\n2 - playlist\n')
 
@@ -18,11 +19,13 @@ def main():
         print(f'Total video --- {len(links)}\n')
         Thread(target=download_video, args=(link, index, )).start()
 
+
 def download_video(link, number):
     video = YouTube(link)
     stream = video.streams.get_highest_resolution()
     stream.download('video/')
     print('\t' + str(number) + ' --- finish')
+
 
 if __name__ == '__main__':
     main()
